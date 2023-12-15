7de06e50b7831be82aa9930156f2c286573d92c7
JairoGarciaF <jairo.dgf@hotmail.com>
2023-12-15 16:53:58 -0500

    BListView completo

--- 02-Android/app/build.gradle.kts
+++ 02-Android/app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.example.b2023_gr1sw_jdgf"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.b2023_gr1sw_jdgf"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- 02-Android/app/src/main/java/com/example/b2023_gr1sw_jdgf/BListView.kt
+++ 02-Android/app/src/main/java/com/example/b2023_gr1sw_jdgf/BListView.kt
@@ -1,14 +1,21 @@
 package com.example.b2023_gr1sw_jdgf
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.snackbar.Snackbar
 
 class BListView : AppCompatActivity() {
     val arreglo = BBaseDatosMemoria.arregloBEntrenador
-
+    var posicionItemSeleccionado = -1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_blist_view)
@@ -26,8 +33,80 @@ class BListView : AppCompatActivity() {
         botonAnadirListView.setOnClickListener {
             anadirEntrenador(adaptador)
         }
-
+        registerForContextMenu(listView)
     }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        //Llnamos las opciones del menu
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        // Obtener el id del ArrayListSeleccionado
+        val info = menuInfo as AdapterView.AdapterContextMenuInfo
+        val posicion = info.position
+        posicionItemSeleccionado = posicion
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.mi_editar ->{
+                mostrarSnackbar("${posicionItemSeleccionado}")
+                return true
+            }
+            R.id.mi_eliminar ->{
+                mostrarSnackbar("${posicionItemSeleccionado}")
+                abrirDialogo()
+                return true
+            }
+            else -> super.onContextItemSelected(item)
+        }
+    }
+
+    fun abrirDialogo(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Desea eliminar")
+        builder.setPositiveButton(
+            "Aceptar",
+            DialogInterface.OnClickListener {
+                    dialog, which -> mostrarSnackbar("Eliminar aceptado")
+            }
+        )
+        builder.setNegativeButton(
+            "Cancelar",
+            null
+        )
+        val opciones = resources.getStringArray(
+            R.array.string_array_opciones_dialogo
+        )
+        val seleccionPrevia = booleanArrayOf(
+            true,
+            false,
+            false
+        )
+        builder.setMultiChoiceItems(
+            opciones,
+            seleccionPrevia,
+            { dialog,
+              which,
+              isChecked -> mostrarSnackbar("Dio clic en el item ${which}")
+            }
+        )
+        val dialogo = builder.create()
+        dialogo.show()
+    }
+
+    fun mostrarSnackbar(texto:String){
+        Snackbar.make(
+            findViewById(R.id.lv_list_view),
+            texto,
+            Snackbar.LENGTH_LONG
+        ).show()
+    }
+
     fun anadirEntrenador(
         adapador: ArrayAdapter<BEntrenador>
     ){
